24ff0dd2fbeb55a1df34d5d155c7b58ed05b8a97
zhengxinhan <chengfengke@bupt.edu.cn>
2024-04-25 14:43:55 +0800

    login page get userprofile

--- pages/login/login.js
+++ pages/login/login.js
@@ -7,6 +7,16 @@ Page({
   onLogin: function() {
     // 登录逻辑处理
     if(this.data.checked) {
+      wx.getUserProfile({
+        desc: '用于完善会员资料', // 这里填写一些说明，告知用户为什么需要这些数据
+        success: (res) => {
+          console.log('用户信息', res.userInfo);
+          // 你可以在这里将用户信息发送到后台服务器
+        },
+        fail: () => {
+          // 处理用户拒绝授权的情况
+        }
+      });
     wx.login({
       success(res) {
         console.log(res)
