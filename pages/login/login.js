ed82d045b24b7e0e354483bef1267cfc91bc1339
zhengxinhan <chengfengke@bupt.edu.cn>
2024-04-25 14:30:37 +0800

    Improve the login logic

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,15 +1,16 @@
 Page({
   data: {
     avatarPath: '/images/avatar-XiaoXi.png', // 您会提供的头像路径
+    checked: false,
   },
 
   onLogin: function() {
     // 登录逻辑处理
+    if(this.data.checked) {
     wx.login({
       success(res) {
+        console.log(res)
         if (res.code) {
-          // 发起网络请求，使用res.code到您的服务器获取session_key和openid
-          // ...处理服务器返回的数据
         } else {
           wx.showToast({
             title: '登录失败',
@@ -18,5 +19,20 @@ Page({
         }
       }
     });
+    wx.navigateTo({
+      url: '/pages/index/index',
+    })
+  }
+  else {
+    wx.showToast({
+      title: '请先阅读并同意相关条款',
+      icon: 'none'
+    });
+  }
+  },
+  onChange(event) {
+    console.log(event.detail)
+    // event.detail 的值为当前选中项绑定值
+    this.setData({ checked: event.detail });
   }
 });
