f7a1085b085701ebec1c7d495e3aa950e04c0e77
yzz0806 <jlzacg1117@gmail.com>
2024-05-09 16:27:15 +0800

    profile页面的三个弹窗更新

--- pages/identity/identity.js
+++ pages/identity/identity.js
@@ -8,17 +8,71 @@ Page({
     appName:"编辑资料",
     Avatar:"/images/student.png",
     nickname:"bot",
-    gender:"男",
+    gender:" ",
     birthday:" ",
-    district: " ",
-    info:" ",
-      show: false
+    area: " ",
+    info:"...",
+    
+    GenderPopup: false,
+    NamePopup:false,
+    BirthPopop: false,
+    AreaPopop: false,
+    InfoPopop: false,
+    genderOptions:['男','女','不愿透露'],
+
   },
-  methods:{
-    showPopup(){
-      this.show = true;
-    }
+  /*关闭弹窗 */
+  onClose(e){
+    this.setData({
+      GenderPopup: false,
+      NamePopup: false,
+      BirthPopup: false,
+      AreaPopup: false,
+      InfoPopup: false,
+    });
   },
+
+  /*昵称弹窗*/
+  showNamePopop(){
+    this.setData({
+      NamePopup:true,
+    })
+  },
+  onNicknameInput(e) {
+    // console.log(e);
+    this.setData({ nickname: e.detail });
+  },
+  
+  /*性别弹窗 */
+  showGenderPopup(e) {
+    this.setData({ GenderPopup: true });
+  },
+
+  onGenderChange(e){ //切换选项
+    console.log(e);
+    // const { value } = e.detail;
+    this.setData({ gender: e.detail.value});
+
+    setTimeout(()=>{ //选完关闭弹窗时有一个小延时
+      this.setData({GenderPopup: false});
+    }, 100);
+  },
+
+  showBirthPopup(e){
+    this.setData({ BirthPopup: true})
+  },
+
+  showAreaPopup(e) {
+    this.setData({ AreaPopup: true });
+  },
+
+  showInfoPopup(e) {
+    this.setData({ InfoPopup: true });
+  },
+  onInfoInput(e){
+    this.setData({ info: e.detail });
+  },
+
   navigateToProfile: function() {
     wx.navigateTo({
       url: '/pages/profile/profile',
